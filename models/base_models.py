dc4021465fbb120deeeb4d5110bd258a856c2997
Gideon Oba <deyonoba@gmail.com>
2024-02-10 09:48:49 +0000

    modified: base_model.py add print tests

--- models/base_models.py
+++ models/base_models.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+import sys
+import os
+
+os.chdir("/Users/Hp/RunVagrant/AirBnB_clone")
+print(os.getcwd())
+# Add the current directory to sys.path
+sys.path.append(os.getcwd())
+print(sys.path)
 
 import models
 import uuid
@@ -26,6 +34,7 @@ class BaseModel():
 
     def save(self):
         self.updated_at = datetime.datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -33,34 +42,49 @@ class BaseModel():
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         my_dict["__class__"] = self.__class__.__name__
+        print("my_dict")
+        print(my_dict)
         return (my_dict)
 
 
 if __name__ == "__main__":
+    # my_model = BaseModel()
+    # my_model.name = "My_First_Model"
+    # my_model.my_number = 89
+    # print(my_model.id)
+    # print(my_model)
+    # print(type(my_model.created_at))
+    # print("--")
+    # my_model_json = my_model.to_dict()
+    # print(my_model_json)
+    # print("JSON of my_model:")
+    # for key in my_model_json.keys():
+    #     print(
+    #             "\t{}: ({}) - {}".format(
+    #                 key,
+    #                 type(my_model_json[key]),
+    #                 my_model_json[key])
+    #             )
+    # print("--")
+    # my_new_model = BaseModel(**my_model_json)
+    # print(my_new_model.id)
+    # print(my_new_model)
+    # print(type(my_new_model.created_at))
+
+    # print("--")
+    # print(my_model is my_new_model)
+    # model_3 = my_model
+    # print(my_model is model_3)
+    from models import storage
+    all_objs = storage.all()
+    print("-- Reloaded objects --")
+    for obj_id in all_objs.keys():
+        obj = all_objs[obj_id]
+        print(obj)
+
+    print("-- Create a new object --")
     my_model = BaseModel()
     my_model.name = "My_First_Model"
     my_model.my_number = 89
-    print(my_model.id)
+    my_model.save()
     print(my_model)
-    print(type(my_model.created_at))
-    print("--")
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print(
-                "\t{}: ({}) - {}".format(
-                    key,
-                    type(my_model_json[key]),
-                    my_model_json[key])
-                )
-    print("--")
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model.id)
-    print(my_new_model)
-    print(type(my_new_model.created_at))
-
-    print("--")
-    print(my_model is my_new_model)
-    model_3 = my_model
-    print(my_model is model_3)
