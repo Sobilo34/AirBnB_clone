14c0b9a1ec60a7189dbb8fa25dd60ce147e5b537
Gideon Oba <deyonoba@gmail.com>
2024-02-11 10:06:36 +0100

    fix: remove the attribute __class__ from instance dictionary returned

--- models/base_models.py
+++ models/base_models.py
@@ -42,5 +42,4 @@ class BaseModel():
         my_dict = self.__dict__.copy()
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        my_dict["__class__"] = self.__class__.__name__
         return my_dict
