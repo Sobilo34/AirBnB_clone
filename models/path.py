923bfb91fca16da36afacfb963004ace33f271ca
Sobilo34 <bilalsolih60@gmail.com>
2024-02-12 06:26:24 -0800

    deleted two files

--- models/path.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-# Iterate over sys.path and modify the elements
-sys.path = [path.replace('/models', '') if path.startswith('/AirBnB_clone/models') else path for path in sys.path]
-
-# Print the modified sys.path
-print(sys.path)
