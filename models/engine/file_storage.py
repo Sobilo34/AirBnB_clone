f7d0a587218d75b1e638d21192cec895cc71354b
Sobilo34 <bilalsolih60@gmail.com>
2024-02-11 18:40:25 -0800

    added __init__ to file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,9 +7,11 @@ class FileStorage:
     """Serializes instances to a JSON file and deserializes
 JSON file to instances.
     """
-
-    __file_path = "file.json"
-    __object = {}
+    def __init__(self):
+        """Initialize FileStorage."""
+        self.__file_path = "file.json"
+        self.__object = {}
+        self.reload()
 
     def all(self):
         """Returns the dictionary __objects"""
