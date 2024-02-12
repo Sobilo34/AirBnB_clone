9a6cf35291138c5565dfce482eb67f8eabe444b3
Sobilo34 <bilalsolih60@gmail.com>
2024-02-11 18:15:25 -0800

    restote file_storage back to initial

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
 """Helps recreate BaseModel class from a dictionary representation."""
-
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.city import City
-from models.review import Review
-from models.state import State
-from models.amenity import Amenity
 
 
 class FileStorage:
@@ -18,14 +10,6 @@ JSON file to instances.
 
     __file_path = "file.json"
     __object = {}
-    __class_list = {
-            "User": User,
-            "Place": Place,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Review": Review
-            }
 
     def all(self):
         """Returns the dictionary __objects"""
@@ -47,12 +31,12 @@ JSON file to instances.
 
     def reload(self):
         """Deserialize object from __file_path"""
+        from models.base_model import BaseModel 
         try:
             with open(FileStorage.__file_path) as file:
                 json_dict = json.load(file)
                 for key, obj_dict in json_dict.items():
-                    obj_name = obj_dict.pop("__class__", "BaseModel")
-                    obj_list = FileStorage.__class_list.get(obj_name, BaseModel)
-                    self.new(obj_list(**obj_dict))
+                    obj_name = key.split(".")[0]
+                    self.new(eval(obj_name)(**obj_dict))
         except FileNotFoundError:
             pass
