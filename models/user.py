5601a1a21e7763b1b1a9f5a4822190e10f623e9a
Gideon Oba <deyonoba@gmail.com>
2024-02-12 22:57:38 +0000

    fix: user.py fix __str__ and to_dict methods

--- models/user.py
+++ models/user.py
@@ -13,3 +13,25 @@ class User(BaseModel):
     password: str = ""
     first_name: str = ""
     last_name: str = ""
+
+    def __init__(self):
+        """Class initialization."""
+        super().__init__()
+        self.email = User.email
+        self.password = User.password
+        self.first_name = User.first_name
+        self.last_name = User.last_name
+
+    def __str__(self):
+        """String representation of Class instance."""
+        str_repr = "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
+        return str_repr
+
+    def to_dict(self):
+        """Converts instance attributes to a dictionary."""
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
